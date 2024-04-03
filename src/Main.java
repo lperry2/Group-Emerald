69049bbefa0f90a1f42b6f7dedab5ae56aff0801
Lucas Perry <lperry2@uw.edu>
2024-04-02 19:02:30 -0700

    Create Empty Frame
    Just a frame, no panels.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 package src;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
-
-
-
-
+        JFrame emeraldFrame = new JFrame("Emerald");
+        emeraldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        emeraldFrame.setVisible(true);
+        emeraldFrame.setResizable(true);
+        emeraldFrame.setSize(800, 600);
     }
 }
 
