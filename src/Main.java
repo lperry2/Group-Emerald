53000e328e037486830ff7217d50ebaaad1a1459
danisan <danisan@uw.edu>
2024-04-02 19:23:46 -0700

    Added button frame and behavior

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 package src;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,5 +11,14 @@ public class Main {
         emeraldFrame.setVisible(true);
         emeraldFrame.setResizable(true);
         emeraldFrame.setSize(800, 600);
+        JButton closeButton = new JButton("Close");
+        class myListener implements ActionListener {
+            public void actionPerformed(final ActionEvent theEvent) {
+                System.exit(0);
+            }
+        };
+
+        closeButton.addActionListener(new myListener());
+        emeraldFrame.add(closeButton);
     }
 }
