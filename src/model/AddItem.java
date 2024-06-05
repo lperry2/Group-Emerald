405ba2d3f52ef2c813c5194d521dcc2862e65f32
danisan <danisan@uw.edu>
2024-06-05 10:06:53 -0700

    makin it look better

--- src/model/AddItem.java
+++ src/model/AddItem.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 
+//A class
 public class AddItem extends JFrame {
     /** A ToolKit. */
     private static final Toolkit KIT = Toolkit.getDefaultToolkit();
