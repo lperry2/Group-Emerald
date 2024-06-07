1f0bb7a707c074b52197d5c45c08d321798e35be
Owen Orlic <oorlic14@uw.edu>
2024-06-06 18:48:24 -0700

    makin it look better

--- src/model/SingleFile.java
+++ src/model/SingleFile.java
@@ -2,7 +2,11 @@ package src.model;
 
 import java.io.File;
 
-/** Assosiates a string with a file. */
+/**
+ * Associates a string with a file.
+ *
+ * @author Owen Orlic
+ */
 public class SingleFile {
 
     /** The file name. */
@@ -50,7 +54,5 @@ public class SingleFile {
     public void setFile(File theFile) {
         myFile = theFile;
     }
-
-
 
 }
